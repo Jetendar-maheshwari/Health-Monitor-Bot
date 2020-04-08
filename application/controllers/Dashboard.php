8ff880d4ccc4c57de447bfda96c98f410230d110
maheshwari_jetendar <jk12cs80@gmail.com>
2020-04-08 11:54:16 +0200

    Layout Changes and Bug Resolved

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -180,7 +180,6 @@ class Dashboard extends CI_Controller {
         $this->form_validation->set_rules('sex', display('sex'),'required|max_length[10]');
         $this->form_validation->set_rules('date_of_birth', display('date_of_birth'),'max_length[10]');
         $this->form_validation->set_rules('address',display('address'),'required|max_length[255]');
-        $this->form_validation->set_rules('status',display('status'),'required');
         #-------------------------------#
         //picture upload
         $picture = $this->fileupload->do_upload(
